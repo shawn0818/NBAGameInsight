03f9ea1d3c8eef21c909bf6e539ee18357a79fea
Shawn <hanxiang007@gmail.com>
2023-12-28 21:13:10 +0800

    这个版本继续重写了GameDataParser，GameDataDisplay，NBADataProcessor，但是能够完成的功能很少，只能使用显示简单比赛信息这个方法，不过能够跑起来了。后续可以进行功能上的具体优化。百米赛跑，现在是终于启动了，跑出了几米。

--- lebronbot.py
+++ lebronbot.py
@@ -155,165 +155,118 @@ class GameDataFetcher:
         endpoint = "boxscore/boxscore"
         return self.fetch_data(endpoint)
 
-#获取数据之后进行数据解析
+#获取比赛本身实时统计数据之后进行数据解析
 class GameDataParser:
     
-    def __init__(self):
-        # 初始化时设置属性
-        self.game_info_df = None
-        self.home_all_df = None
-        self.away_all_df = None
-        self.home_player_df = None
-        self.away_player_df = None
-
-    @staticmethod
-    def convert_utc_to_local(utc_time_str, local_tz='Asia/Shanghai'):
-        utc_time = datetime.strptime(utc_time_str, "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=timezone('UTC'))
-        local_time = utc_time.astimezone(timezone(local_tz))
-        return local_time.strftime('%Y-%m-%d %H:%M:%S')
-      
-    def parse_game_data(self,game_data):
-        game_df = pd.json_normalize(game_data,errors="ignore",sep='_')
-        self.game_info_df = game_df.iloc[:, 4:24]  
-        self.home_all_df = game_df.iloc[:, 26:96] 
-        self.away_all_df =  game_df.iloc[:, 97:166] 
+    #获取关于比赛本身的一些信息，比如比赛进行状态，比赛时间地点场馆，本场执法裁判等
+    def parse_game_data(self,boxscore_data):
         
-        home_record_path = ["game","homeTeam","players"]
-        home_meta = [["game","homeTeam","teamId"],["game","homeTeam","teamName"],["game","homeTeam","teamCity"],["game","homeTeam","teamTricode"],["game","homeTeam","score"]]
-        self.home_player_df = pd.json_normalize(game_data,record_path=home_record_path,meta=home_meta,errors="ignore",sep='_')
-
-        away_record_path = ["game","awayTeam","players"]
-        away_meta = [["game","awayTeam","teamId"],["game","awayTeam","teamName"],["game","awayTeam","teamCity"],["game","awayTeam","teamTricode"],["game","awayTeam","score"]]
-        self.away_player_df = pd.json_normalize(game_data,record_path=away_record_path,meta=away_meta,errors="ignore",sep='_')
+        game_boxsocre_df = pd.json_normalize(boxscore_data,errors="ignore",sep='_')
         
-       
-    def parse_game_info(self,game_info_data):
-            
-        game_info_data = self.parse_game_data().game_info_df
-        # game_info['gameTimeUTC'] 是一个字符串，包含了 UTC 时间
-        utc_time_str = game_info['gameTimeUTC']
-        #转成本地北京时间
-        game_time_beijing = GameDataParser.convert_utc_to_local(utc_time_str)                         
-        #比赛地点
-        game_place_city = game_info['arena']["arenaCity"]
-        game_place_arena = game_info['arena']["arenaName"]
-        #比赛双方
-        home_team_name = game_info["homeTeam"]["teamName"]
-        away_team_name = game_info["awayTeam"]["teamName"]
-        # 判断查询的 team_id 是主队还是客队
-        team_role = None
-        team_role = None
-        home_team_role = "主场"
-        away_team_role = "客场"            
-        if game_info["homeTeam"]["teamId"] == team_id:
-            team_role = home_team_role
-        elif game_info["awayTeam"]["teamId"] == team_id:
-            team_role = away_team_role          
-        #最终需要的比赛信息
-        game_info_parsed = {
-        "time": game_time_beijing,
-        "place_city": game_place_city,
-        "place_arena": game_place_arena,
-        "home_team": f"{home_team_name} ({home_team_role})",
-        "away_team": f"{away_team_name} ({away_team_role})",
-        "team_role": team_role
-        }
-
-        return game_info_parsed
-            
-    def parse_realtime_score(self, score_data):
-        if score_data:
-            # 这里假设'score_data' JSON 结构中有'homeTeam'和'awayTeam'的得分信息
-            home_team = score_data['game']['homeTeam']
-            away_team = score_data['game']['awayTeam']
-
-            #获取比赛状态，比赛目前进行到第几节
-            game_status = score_data["game"]['gameStatus']
-            game_period = score_data["game"]['period']
-            game_clock =  score_data["game"]['gameClock']
-            # 使用正则表达式来匹配分钟和秒
-            matches = re.match(r"PT(\d{2})M(\d{2})S", game_clock)
-            # 提取分钟和秒，现在 minutes 和 seconds 包含了剩余的分钟和秒数
-            game_clock_minutes = matches.group(1)
-            game_clock_seconds = matches.group(2)
-
-            #双方球队目前得分
-            home_score = home_team["score"]
-            away_score = away_team["score"]
-
-            #球队统计数据，不止是得分维度
-            team_statistics_df = pd.concat([
-                self.build_team_statistics_df(home_team),
-                self.build_team_statistics_df(away_team)
-            ], ignore_index=True)
-
-            #球员统计数据，不止是得分维度
-            player_statistics_df = pd.concat([
-                self.build_player_statistics_df(home_team["players"], 'Home'),
-                self.build_player_statistics_df(away_team["players"], 'Away')
-            ], ignore_index=True)
-
-            realtime_box_score = {
-                "game_status":game_status,
-                "game_period":game_period,
-                "game_clock_minutes":game_clock_minutes,
-                "game_clock_seconds":game_clock_seconds,
-                "home_score":home_score,
-                "away_score":away_score
-            }
-            
-            return realtime_box_score,team_statistics_df,player_statistics_df 
- 
-    def parse_gameover_score(self, score_data):
-
-        if score_data:
-            # 这里假设'score_data' JSON 结构中有'homeTeam'和'awayTeam'的得分信息
-            home_team = score_data['game']['homeTeam']
-            away_team = score_data['game']['awayTeam']
-
-            #球队统计数据，不止是得分维度
-            team_statistics_df = pd.concat([
-                self.build_team_statistics_df(home_team),
-                self.build_team_statistics_df(away_team)
-            ], ignore_index=True)
-
-            #球员统计数据，不止是得分维度
-            player_statistics_df = pd.concat([
-                self.build_player_statistics_df(home_team["players"], 'Home'),
-                self.build_player_statistics_df(away_team["players"], 'Away')
-            ], ignore_index=True)
-
-            return team_statistics_df,player_statistics_df 
+        game_start_position = game_boxsocre_df.columns.get_loc('game_gameId')
+        game_end_position = game_boxsocre_df.columns.get_loc('game_officials')
+        game_info_df = game_boxsocre_df.iloc[:, game_start_position:game_end_position+1] 
+        game_info_df.columns = game_info_df.columns.str.split('_').str[-1]
+             
+        #需要添加关于球队的列的信息
+        team_columns_df = game_boxsocre_df.loc[:, ['game_homeTeam_teamId', 'game_homeTeam_teamName','game_awayTeam_teamId', 'game_awayTeam_teamName']]
+        #简化下列名
+        team_columns_df.columns = team_columns_df.columns.str.extract(r'game_(.*?)_(.*)').agg('_'.join, axis=1)
         
-    def parse_playbyplay(self, actions_data):
+        game_board_df = pd.concat([game_info_df, team_columns_df], axis=1)
+             
+        return game_board_df
+                
+    #解析关于球队的数据，比如球队整体的数据统计       
+    def parse_team_data(self,boxscore_data):
+        
+        game_boxsocre_df = pd.json_normalize(boxscore_data,errors="ignore",sep='_')
+        
+        home_start_position = game_boxsocre_df.columns.get_loc('game_homeTeam_teamId')
+        home_end_position = game_boxsocre_df.columns.get_loc('game_homeTeam_statistics_twoPointersPercentage')
+        home_team_df = game_boxsocre_df.iloc[:, home_start_position:home_end_position+1] 
+
+        away_start_position = game_boxsocre_df.columns.get_loc('game_awayTeam_teamId')
+        away_end_position = game_boxsocre_df.columns.get_loc('game_awayTeam_statistics_twoPointersPercentage')
+        away_team_df = game_boxsocre_df.iloc[:, away_start_position:away_end_position+1]  
+        
+        #修改列名一致，方便进行下一步拼接
+        home_team_df.columns = home_team_df.columns.str.split('_').str[-1]
+        away_team_df.columns = away_team_df.columns.str.split('_').str[-1]      
+        # 进行上下拼接
+        team_df = pd.concat([home_team_df, away_team_df], ignore_index=True)
+        # 在原始数据框上就地去掉不用的列
+        team_df.drop(["periods","players"], axis=1, inplace=True)
+        
+        return team_df
+    
+    #解析球员的实时数据统计      
+    def parse_player_data(self, boxscore_data):
+        
+        #解析出球员数据        
+        home_player_df = pd.json_normalize(boxscore_data,record_path= ["game","homeTeam","players"],meta=[["game","homeTeam","teamId"],["game","homeTeam","teamName"]],errors="ignore",sep='_')
+        away_player_df = pd.json_normalize(boxscore_data,record_path= ["game","awayTeam","players"],meta=[["game","awayTeam","teamId"],["game","awayTeam","teamName"]],errors="ignore",sep='_')
+        
+        #使 meta 部分添加的列名保持一致
+        home_player_df =home_player_df.rename(columns={'game_homeTeam_teamId': 'teamId', 'game_homeTeam_teamName': 'teamName'})
+        away_player_df =away_player_df.rename(columns={'game_awayTeam_teamId': 'teamId', 'game_awayTeam_teamName': 'teamName'})
+
+        # 进行上下拼接
+        player_df = pd.concat([home_player_df, away_player_df], ignore_index=True)
+
+        return player_df
+                
+    #解析本场比赛的所有回合，从回合中可以得到更详细的数据，比如投篮地点，投篮方式等等。                 
+    def parse_playbyplay_data(self, actions_data):
+        
         if actions_data:
-            actions_df = pd.json_normalize(actions_data, record_path=['game', 'actions'])
+            
+            actions_df = pd.json_normalize(actions_data, record_path=['game', 'actions'],meta=[["game","gameId"]],errors="ignore",sep='_')
+            
             return actions_df
         
 
 class GameDataDisplay:
-
-    @staticmethod
-    def display_game_info(game_info_parsed):
-
-        if game_info_parsed:
-            game_info_text = (
-            "【今日比赛】 \n"
-            f"时间：北京时间 {game_info_parsed['time']}\n"
-            f"地点：{game_info_parsed['place_city']}，{game_info_parsed['place_arena']}\n"
-            f"比赛双方：{game_info_parsed['home_team']} vs {game_info_parsed['away_team']}"
-            )
-            return game_info_text
-        return None
     
     @staticmethod
-    def display_realtime_score(score_data_parsed):         
-        pass      
+    def display_game_info(game_info_parsed):
+        
+        #转化下比赛时间的时区
+        game_info_parsed["gameTimeBeijing"] =  pd.to_datetime(game_info_parsed["gameTimeUTC"]).dt.tz_convert('Asia/Shanghai').dt.strftime('%Y-%m-%d %H:%M:%S')
+        game_info_dict = game_info_parsed.to_dict(orient='records')[0]
+
+        if game_info_dict["gameStatus"] == 1:
+            game_info_text = (
+            "【比赛信息】 \n"
+            f"时间：北京时间 {game_info_dict['gameTimeBeijing']}\n"
+            f"地点：{game_info_dict['arenaCity']}，{game_info_dict['arenaName']}\n"
+            f"比赛双方：{game_info_dict['homeTeam_teamName']} vs {game_info_dict['awayTeam_teamName']}"
+            )        
+        elif game_info_dict["gameStatus"] == 2 :          
+            game_info_text = f"比赛已经在北京时间 {game_info_dict['gameTimeBeijing']}开始了，快打开电视收看吧"                
+        else:
+            game_info_text = f"比赛已经在北京时间 {game_info_dict['gameTimeBeijing']}开始了，现在已经结束啦"                      
+        return game_info_text
+    
+    @staticmethod
+    def display_realtime_score(score_data_parsed):    
+        
+        player_id = None
+        player_df = score_data_parsed  
+        player_filter = player_df["personId"] == int(player_id)
+        one_player_df = player_df[player_filter]
+              
+        return one_player_df         
    
     @staticmethod
     def display_gameover_score(score_data_parsed):
-        pass
-   
+        
+        team_id = None
+        team_df = score_data_parsed  
+        team_filter =team_df["teamId"] == int(team_id)
+        one_team_df = team_df[team_filter]
+        return one_team_df
+         
     @staticmethod
     def display_playbyplay(actions_data_parsed):
         #这里主要是想解析出来球员的得分篮板助攻盖帽抢断等回合
@@ -336,40 +289,26 @@ class GameDataDisplay:
 class NBADataProcessor:
 
     #展示比赛基本信息
+    @staticmethod
     def process_and_present_game_data(game_date, team_id):
         
         #先实例化 NBASchedule 类，筛选获得对应日期的比赛
-        schedule_fetcher = NBASchedule() 
-        team_schedule =  schedule_fetcher.filter_team_schedule(game_date=game_date, team_id=team_id)
-        
-         
-        if not team_schedule.empty:
-            
-            #如果存在比赛的话，找到关键的 game_id
-            game_id = team_schedule["gameId"].iloc[0]
-            
-            # 通过我们 game_id，现在实例化 GameDataFetcher
-            game_data_fetcher = GameDataFetcher(game_id)            
-            game_data = game_data_fetcher.fetch_boxscore()
-            
-            #实例化 GameDataparser
-            game_data_parser = GameDataParser()
-            game_data_parsed = game_data_parser.parse_game_info(team_id=team_id,game_info_data=game_data)
-            
-            #实例化 GameDataDisplay
-            game_data_display = GameDataDisplay.display_game_info(game_data_parsed)
-            print(game_data_display)
-            return game_data_display
-        else:
-            print("【今日无比赛】")
-
-    
+        team_schedule_df = NBASchedule().filter_team_schedule(team_id=team_id,game_date=game_date)
+        team_schedule_dict = TeamScheduleDataParser.get_schedule_as_dict(team_schedule_df)
+        #得到 gameid
+        game_id = team_schedule_dict["gameId"]
+        game_boxsocre = GameDataFetcher(game_id).fetch_game_boxscore()
+        game_info_df = GameDataParser().parse_game_data(game_boxsocre)
+        game_info_text = GameDataDisplay().display_game_info(game_info_df)      
+        return game_info_text
+ 
 def main():
     team_id = '1610612747'  # 假设这是一个有效的球队 ID
-    game_date = "12/24/2023"  # 假设这是需要查询的比赛日期
+    game_date = "2023-12-08"  # 假设这是需要查询的比赛日期
     processor = NBADataProcessor()
     #展示比赛基本信息
-    processor.process_and_present_game_data(game_date = game_date,team_id=team_id)
+    game_info_text = processor.process_and_present_game_data(game_date,team_id)
+    print(game_info_text)
     
 if __name__ == "__main__":
     main()
